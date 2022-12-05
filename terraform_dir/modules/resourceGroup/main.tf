82f3c32b8a1f8e5dc6e6e1a93f7f51f0ad7828fc
rbharadwaj16 <48885869+rbharadwaj16@users.noreply.github.com>
2022-12-05 17:07:42 +0530

    Feature/fix init pipeline (#15)
    * changed param syntax
    
    * Fixed syntax
    
    * Fixed syntax
    
    * Fixed syntax
    
    * Fixed syntax
    
    * Fixed syntax
    
    * Adding var group
    
    * Changed type of param to object
    
    * Empty Install
    
    * Updated install yaml
    
    * Updated env
    
    * Adding steps into install task
    
    * Adding steps into init and plan task
    
    * Changed task version
    
    * Changed install task
    
    * Adding task version
    
    * testing stage dependency
    
    * Dependency refinement
    
    * Changed SC var
    
    * Passing direct SC
    
    * Passing direct SC
    
    * Adding install in each job
    
    * Removed init depenency
    
    * passing sc directly
    
    * adding download artifact task
    
    * adding download artifact task to plan
    
    * Reverting sc changes
    
    * Applying sc via KV
    
    * Meddling the sc
    
    * Passing vars to plan
    
    * Passing vars to plan
    
    * Restructuring vars
    
    * correcting plan syntax
    
    * playing with tf plan
    
    * playing with tf plan
    
    * Corrected hyphen to underscore
    
    * playing with var groups
    
    * playing with var groups
    
    * trying to get the plan command right
    
    * Restructing job
    
    * Adding auth vars in tf
    
    * Corrected a typo
    
    * Adding rg module
    
    * Playing with rg module for_Each
    
    * Playing with rg module for_Each

--- terraform_dir/modules/resourceGroup/main.tf
+++ terraform_dir/modules/resourceGroup/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
     for_each = var.rg_name
-    name = format("%s_%s_%s_%s", var.owner, each.key, var.env, var.location_short)
+    name = ${var.owner}_${var.env}_${each.value}_$var.location_short
     location = var.location
     lifecycle {
       ignore_changes = [

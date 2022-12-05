13f84b2729c6c08b4826587c06d89cfc8d61f2f9
raghav <rbharadwaj16@gmail.com>
2022-12-05 17:09:15 +0530

    Testing each value

--- terraform_dir/modules/resourceGroup/main.tf
+++ terraform_dir/modules/resourceGroup/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
     for_each = var.rg_name
-    name = ${var.owner}_${var.env}_${each.value}_$var.location_short
+    name = each.value
     location = var.location
     lifecycle {
       ignore_changes = [

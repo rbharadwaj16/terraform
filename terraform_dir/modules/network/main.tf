0d1762f552735902f94c9915e2237f0ce5a6d621
raghav <rbharadwaj16@gmail.com>
2022-12-11 00:12:11 +0530

    Fixing network module errors

--- terraform_dir/modules/network/main.tf
+++ terraform_dir/modules/network/main.tf
@@ -7,8 +7,8 @@ data "azurerm_resource_group" "network_rg" {
 resource "azurerm_virtual_network" "vnet" {
   for_each            = var.vnet
   name                = "${var.owner}_${var.env}_${each.value["name"]}_${var.location_short}}"
-  location            = "data.azurerm_resource_group.network_rg.${each.key}.location"
-  resource_group_name = "data.azurerm_resource_group.network_rg.${each.key}.name"
+  location            = data.azurerm_resource_group.network_rg[each.key].location
+  resource_group_name = data.azurerm_resource_group.network_rg[each.key].name
   address_space       = each.value["address_space"]
   lifecycle {
     ignore_changes = [

d52bd61750bc5fdd194b4ef772d546df471489ae
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-08-16 00:06:03 +0530

    feat: Add DNS server configuration to Azure Virtual Network module

--- modules/terraform-azurerm-virtual-network/main.tf
+++ modules/terraform-azurerm-virtual-network/main.tf
@@ -1,7 +1,17 @@
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
   resource_group_name = var.resource_group_name
+  dns_servers         = var.dns_servers
   location            = var.location
   address_space       = var.address_space
   tags                = var.tags
 }
+
+resource "azurerm_subnet" "this" {
+  for_each = var.subnets
+  name = each.value.name
+  address_prefixes = each.value.address_prefix  
+  resource_group_name = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+}
+

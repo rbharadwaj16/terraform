80615a781d9afc9d39d12e952eccfbad34561bf0
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2026-08-15 23:54:42 +0530

    feat: Add main configuration and variables for Azure Virtual Network module (#38)

--- modules/terraform-azurerm-virtual-network/variables.tf
+++ modules/terraform-azurerm-virtual-network/variables.tf
@@ -2,3 +2,34 @@ variable "vnet_name" {
     description = "The name of the virtual network."
     type        = string
 }
+
+variable "resource_group_name" {
+    description = "The name of the resource group in which to create the virtual network."
+    type        = string
+  
+}
+
+variable "location" {
+    description = "The Azure region in which to create the virtual network."
+    type        = string
+}
+
+variable "address_space" {
+    description = "The address space that is used by the virtual network."
+    type        = list(string)
+}
+
+variable "subnets" {
+    description = "A list of subnets to create within the virtual network."
+    type=  map(object({
+        name = string
+        address_prefix = string
+    }))
+}
+
+variable "tags" {
+    description = "A map of tags to assign to the virtual network."
+    type        = map(string)
+    default     = {}
+}
+
